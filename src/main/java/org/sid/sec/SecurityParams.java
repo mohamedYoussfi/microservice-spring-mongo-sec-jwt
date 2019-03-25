6e4ef3486f3cb04bf1898ace60d4c566a994411f
Nidhal Arouri <arrouurii@gmail.com>
2019-03-25 09:43:35 +0100

    EXPIRATION IN MS

--- src/main/java/org/sid/sec/SecurityParams.java
+++ src/main/java/org/sid/sec/SecurityParams.java
@@ -3,6 +3,6 @@ package org.sid.sec;
 public interface SecurityParams {
     public static final String JWT_HEADER_NAME="Authorization";
     public static final String SECRET="med@youssfi.net";
-    public static final long EXPIRATION=10*24*3600;
+    public static final long EXPIRATION=10*24*3600*1000; // in MilliSeconds
     public static final String HEADER_PREFIX="Bearer ";
 }
